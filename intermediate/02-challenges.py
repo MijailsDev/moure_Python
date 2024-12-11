5c2c5053bcb74d4534d803fa56243eda50bfbb2a
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-11 12:45:22 -0500

    Challenges Anagrama resuelto

--- intermediate/02-challenges.py
+++ intermediate/02-challenges.py
@@ -22,3 +22,21 @@ def fizzbuzz():
             print(index)
 
 fizzbuzz()
+
+
+"""
+ES UNA ANAGRAMA ?  
+  Escribe una función que reciba dos palabras (String) y retorne
+  verdadero o falso (Bool) según sean o no anagramas.
+  - Un Anagrama consiste en formar una palabra reordenando TODAS
+    las letras de otra palabra inicial.
+  - NO hace falta comprobar que ambas palabras existan.
+  - Dos palabras exactamente iguales no son anagrama.
+"""
+
+def is_anagram(word_one, word_two):
+    if word_one.lower() == word_two.lower():
+        return False
+    return sorted(word_one.lower()) == sorted(word_two.lower()) # sorted ordena strings en una lista
+
+print(is_anagram("Amor", "Roma"))
