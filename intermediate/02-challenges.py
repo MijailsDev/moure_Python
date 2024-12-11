f8c4ccd5293db6a94d1e7a1e953baefc0ed2eab4
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-11 12:01:05 -0500

    Challenges - FizzBuzz resuelto

--- intermediate/02-challenges.py
+++ intermediate/02-challenges.py
@@ -8,4 +8,17 @@ EL FAMOSO "FIZ BUZZ":
   - Múltiplos de 3 por la palabra "fizz".
   - Múltiplos de 5 por la palabra "buzz".
   - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
- """
+ """ 
+
+def fizzbuzz():
+    for index in range(1,101):
+        if index % 3 == 0 and index % 5 == 0:
+            print("fizzbuzz")
+        elif index % 3 == 0:
+            print("fizz")
+        elif index % 5 == 0:
+            print("buzz")            
+        else:            
+            print(index)
+
+fizzbuzz()
