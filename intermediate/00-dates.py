ad313a4988ab63f0eeccff6fc9543aca08d4b5d7
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-11 09:12:52 -0500

    Date con funciones

--- intermediate/00-dates.py
+++ intermediate/00-dates.py
@@ -4,13 +4,20 @@ from datetime import datetime
 
 now = datetime.now()    # inicializamos un objeto y lo almacenanos en la variable now
 
+def print_date(date):
+    print(date.year)
+    print(date.month)
+    print(date.day)
+    print(date.hour)
+    print(date.minute)
+    print(date.second)
+    print(date.timestamp())
 
-# timestamp
-timestamp = now.timestamp() # es un representacion unica de un tiempo 
+print_date(now)
 
-print(timestamp)
- 
 
 # crear una nueva fecha para el nuevo año
-year_2023 = datetime(2023, 1, 1)
+year_2023 = datetime(2023, 1, 1)    # para el año se necesita minimo los tres datos : año mes dia
 
+print_date(year_2023)
+ 
