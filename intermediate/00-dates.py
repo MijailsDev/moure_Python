27f53cf57a34cfd6b8d705c5e008fc2345cb1ba0
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-11 09:06:44 -0500

    Date - timestamp

---
+++ intermediate/00-dates.py
@@ -0,0 +1,16 @@
+### Datos ###
+
+from datetime import datetime
+
+now = datetime.now()    # inicializamos un objeto y lo almacenanos en la variable now
+
+
+# timestamp
+timestamp = now.timestamp() # es un representacion unica de un tiempo 
+
+print(timestamp)
+ 
+
+# crear una nueva fecha para el nuevo año
+year_2023 = datetime(2023, 1, 1)
+
