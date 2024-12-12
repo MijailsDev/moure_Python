c899f239a0e738cffa76e386e2912513bc9f9af6
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-12 11:25:22 -0500

    Error - IndexError

--- intermediate/05-error-types.py
+++ intermediate/05-error-types.py
@@ -10,3 +10,10 @@ language = "Spanish"    # Comentar para Erro
 print(language)
 
 
+# IndexError
+my_list = ["Python", "Swift", "Kotlin", "Dart", "JavaScript"]
+print(my_list[0])
+print(my_list[4])
+print(my_list[-1])
+# print(my_list[5])     # Descomentar para Error
+
