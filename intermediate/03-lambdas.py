55b3adb6341e448ecd5bae53fe0de7c6cd55770e
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-12 08:56:15 -0500

    Lambda - declaracion

---
+++ intermediate/03-lambdas.py
@@ -0,0 +1,11 @@
+### Lambdas ###
+
+"""
+        son como funciones, las lambdas van a ser
+        funciones anonimas
+
+        una lambda lo puedes almacenar en una variable
+""" 
+
+# Declaracion de una lambda
+sum_two_values = lambda first_value, second_value: first_value + second_value    # esta funcion suma dos valores
