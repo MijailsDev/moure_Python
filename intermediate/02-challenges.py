b2b3208eded3c5cb48dc2d7ecf417b3847c9e9e8
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-12 08:49:44 -0500

    Challenges - Invirtiendo cadenas

--- intermediate/02-challenges.py
+++ intermediate/02-challenges.py
@@ -89,3 +89,25 @@ def is_prime():
                 print(number)
 
 is_prime()
+
+
+
+
+
+
+"""
+INVIRTIENDO CADENAS
+  Crea un programa que invierta el orden de una cadena de texto
+  sin usar funciones propias del lenguaje que lo hagan de forma automática.
+  - Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
+"""
+
+def reverse(text):
+    text_len = len(text)
+    reversed_text = ""
+    for index in range(0, text_len):
+        reversed_text += text[text_len - index - 1]
+    return reversed_text
+
+
+print(reverse("Hola mundo"))
