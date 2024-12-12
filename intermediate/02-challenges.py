8c4ab0764229bc7590b41bb152a5e0cd39ccd980
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-12 08:19:22 -0500

    Challenges - Fibonacci

--- intermediate/02-challenges.py
+++ intermediate/02-challenges.py
@@ -40,3 +40,27 @@ def is_anagram(word_one, word_two):
     return sorted(word_one.lower()) == sorted(word_two.lower()) # sorted ordena strings en una lista
 
 print(is_anagram("Amor", "Roma"))
+
+
+
+"""
+LA SUCESION DE FIBONACCI
+  Escribe un programa que imprima los 50 primeros números de la sucesión
+  de Fibonacci empezando en 0.
+  - La serie Fibonacci se compone por una sucesión de números en
+    la que el siguiente siempre es la suma de los dos anteriores.
+    0, 1, 1, 2, 3, 5, 8, 13...
+"""
+
+def fibonacci():
+    prev = 0
+    next = 1
+
+    for index in range(0, 50):
+        print(prev)
+        fib = prev + next
+        prev = next
+        next = fib
+
+fibonacci()
+
