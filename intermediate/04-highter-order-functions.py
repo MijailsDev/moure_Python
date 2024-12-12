87f24859274ed9f9346dcd7f6e06c24ee4721082
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-12 10:50:40 -0500

    Closures

--- intermediate/04-highter-order-functions.py
+++ intermediate/04-highter-order-functions.py
@@ -17,3 +17,19 @@ def sum_two_values_and_add_value(first_value, second_value, f_sum):
 
 print(sum_two_values_and_add_value(5, 2, sum_one))
 
+
+
+### Closures ###
+
+def sum_ten(original_value):  # Define una función llamada sum_ten que toma un parámetro original_value.
+    def add(value):  # Define una función interna llamada add que toma un parámetro value.
+        return value + 10 + original_value  # La función add devuelve el resultado de sumar value, 10 y original_value.
+    return add  # La función sum_ten devuelve la función add.
+
+add_closure = sum_ten(1)  # Llama a sum_ten con el argumento 1 y asigna la función resultante a add_closure.
+print(add_closure(5))  # Llama a add_closure con el argumento 5, lo que resulta en 5 + 10 + 1 = 16, e imprime 16.
+print((sum_ten(5))(1))  # Llama a sum_ten con el argumento 5, lo que devuelve una función. Luego, llama a esa función con el argumento 1, lo que resulta en 1 + 10 + 5 = 16, e imprime 16.
+
+
+
+
