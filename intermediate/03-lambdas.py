952d6640a08dff185bf8265c7237948c304bf755
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-12 09:41:46 -0500

    Lambda - Probando funciones

--- intermediate/03-lambdas.py
+++ intermediate/03-lambdas.py
@@ -15,3 +15,15 @@ sum_two_values(2, 4)
 
 # manera par mostrar en consola
 print(sum_two_values(2, 4))
+
+
+# Creando otra lambda
+multiply_values = lambda first_value, second_value: first_value * second_value - 3
+print(multiply_values(2, 4))
+
+
+
+def sum_three_values(value):
+    return lambda first_value, second_value: first_value + second_value + value 
+
+print(sum_three_values(5)(2, 4))
