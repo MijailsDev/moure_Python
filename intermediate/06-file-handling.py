567423fdc7ca4d5accb0bd8b1c10f2db97adf618
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-13 08:24:14 -0500

    Archivos .json

--- intermediate/06-file-handling.py
+++ intermediate/06-file-handling.py
@@ -24,3 +24,33 @@ txt_file.close()
 
 with open("intermediate/my-file.txt", "a") as my_other_file:
     my_other_file.write("\nY Swift")
+
+
+
+# .json file
+import json
+ 
+json_file = open("intermediate/my-file.json", "w+") # crea el fichero .json
+
+json_test = {
+    "name":"Brais",
+    "surname":"Moure",
+    "age":"35",
+    "language":["Python", "Swift", "Kotlin"],
+    "website":"https://moure.dev"
+}
+
+json.dump(json_test, json_file, indent=2)
+
+
+json_file.close()
+
+with open("intermediate/my-file.json") as my_other_file:
+    for line in my_other_file.readlines():
+        print(line)
+
+json_dict = json.load(open("intermediate/my-file.json"))
+print(json_dict)
+print(type(json_dict))
+print(json_dict["name"])
+
