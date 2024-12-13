03876f572289c090ea785846a2b24aea64068d9f
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-13 13:05:55 -0500

    Python Package Manager - numpy

---
+++ intermediate/08-python-package-manager.py
@@ -0,0 +1,27 @@
+### Python Package Manager ###
+
+#       Es un gestor de paquetes para Python
+#       Si queremos utilizar algun modulo que no
+#       tenemos descargado entonces debemos de usar esto        
+
+# PIP https://pypi.org
+
+"""
+pip --version
+
+pip install pip
+
+pip install --upgrade pip
+
+pip install numpy
+
+
+
+
+"""
+
+import numpy
+ 
+print(numpy.version.version)
+
+
