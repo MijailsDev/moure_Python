d75fa048f749f9ed10ab770ce8bd7d0a7ef2ee4d
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-13 09:46:33 -0500

    Expresiones regulares - match

---
+++ intermediate/07-regular-expressions.py
@@ -0,0 +1,28 @@
+### Regular Expressions ###
+
+#       Es un estandar para inspeccioar si una cadena 
+#       de texto tiene cosas
+
+import re
+
+# match
+
+my_string = "Esta es la leccion numero 7: Expresione regulares"
+my_other_string = "Esta no es la leccion numero 6: Manejo de ficheros"
+
+# trata de encontrar un patron
+match = re.match("Esta es la leccion", my_string, re.I)
+
+print(match)
+start, end = match.span()
+print(my_string[start:end])
+
+match = re.match("Esta es la leccion", my_other_string)
+#if not(match == None):     # Otra forma de comprobar el None
+#if match != None:       # Otra forma de comprobar el None
+if  match is not None:
+    print(match)
+    start, end = match.span()
+    print(my_other_string[start:end])        
+
+print(re.match("Expresiones regulares", my_string))
