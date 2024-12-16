96e7f401c97e377c12c9c511f0669884304f0c85
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-16 12:13:41 -0500

    Operators comparartivos

--- Basic/03-operadores.py
+++ Basic/03-operadores.py
@@ -16,3 +16,23 @@ print("Hola" * (2 ** 3)) # repite 8 veces Hola
 my_float = 2.5 * 2
 print("Hola" * int(my_float)) # convertimos el 'float' a un entero 'int'
 
+### Operadores Comparativos ###
+# estas lineas nos da el resultado en booleando(True/False)
+print(3 > 4) 
+print(3 < 4)
+print(3 >= 4)
+print(3 <= 4)
+print( 3 == 4)
+print( 3 != 4)
+print( 3 > 4 == 2)
+print(3 > 4 > 2)
+
+# aqui estan compararando ordenacion alfabetica por ASCII
+print("Hola" > "Python")
+print("Hola" < "Python")
+print("Hola" >= "Hola")
+print(len("Hola") >= len("Hola")) # aqui si compara numero de caracteres
+print("Hola" <= "Python")
+print("Hola" == "Python")
+print("Hola" != "Python")
+
