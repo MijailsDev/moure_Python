d52f25df1168ac8eaf7a7c4bf5191721120ecc70
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-16 12:21:38 -0500

    List - Declaracion

---
+++ Basic/05-listas.py
@@ -0,0 +1,18 @@
+### Lists ### 
+# un arreglo es diferente lista#
+# nuestra liste sirve para agregar elemetos, como una caja(0,1,2,3,4,5,6,...)
+# es una forma de agrupar datos  
+#podemos hacer operaciones propias de las listas: ordenaciones, reverseds, injeccion de elemntos
+
+my_list = list()        # declaracion de una lista
+my_other_list = []      # declaracion de una lista
+
+print(len(my_list)) # la longitud es 0 es vacio
+
+my_list = [35, 24, 62, 52, 30, 30, 17] # un array de elementos 
+
+print(my_list)
+print(len(my_list))
+
+my_other_list = [35, 1.77, "Brais", "Moure"]  
+
