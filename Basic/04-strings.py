d5e6bcc01a7c56c49fbb7cc9b18818cb9b515e5a
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-16 12:15:52 -0500

    Strings - saltos de linea

---
+++ Basic/04-strings.py
@@ -0,0 +1,22 @@
+### Strings ###
+
+my_string = "My String"
+my_other_string = 'Mi otro String'
+
+print(len(my_string))
+print(len(my_other_string))
+
+print(len(my_string + " " + my_other_string))
+
+my_new_line_string = "Este es un String \ncon salto de lineal"
+print(my_new_line_string)
+
+my_tab_string = "\tEste es un String con tabulacion"
+print(my_tab_string)
+
+my_scape_string = "\tEste es un String \n escapado"
+print(my_scape_string)
+
+my_scape_string = "\\tEste es un String \\n escapado"
+print(my_scape_string)
+
