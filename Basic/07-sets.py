ed14bde16170e3ca432a0ba1dbf8b9598f04c197
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-16 12:38:56 -0500

    Set - Declaracion - type

---
+++ Basic/07-sets.py
@@ -0,0 +1,13 @@
+### Sets ###
+'''
+tiene de base un array
+un SET no es una estructura ordenada
+un SET no admite repetidos
+'''
+# declaracion de sets
+my_set = set()
+my_other_set = {}
+
+print(type(my_set))
+print(type(my_other_set))   # esto nos dice que inicialmente es un diccionario
+
