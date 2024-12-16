a786a9e0bf543f13d6cb4532f61a292c727139ba
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-16 12:34:12 -0500

    Tuples - Declaracion

---
+++ Basic/06-tuplas.py
@@ -0,0 +1,23 @@
+### Tuples ###
+'''
+Las tuplas son valores constantes, son inmutables
+'''
+
+# Declaracion de tuplas
+my_tuple = tuple()
+my_other_tuple = ()
+
+my_tuple = (35,2.77,"Brais","Moure","Brais")
+my_other_tuple = (35, 60,30)
+
+print(my_tuple)
+print(type(my_tuple))
+
+print(my_tuple[0]) # imprime el elemento en la posicion 0
+
+# imprime el ultimo elemento
+# comienza del ultimo hacia el principio
+print(my_tuple[-1]) 
+print(my_tuple[-2]) 
+print(my_tuple[-3]) 
+
