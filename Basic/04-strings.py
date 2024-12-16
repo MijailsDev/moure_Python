8fd2b9e708b86d5e15de7ea66f525560d07ec7f6
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-16 12:16:24 -0500

    Strings - Formateo

--- Basic/04-strings.py
+++ Basic/04-strings.py
@@ -20,3 +20,12 @@ print(my_scape_string)
 my_scape_string = "\\tEste es un String \\n escapado"
 print(my_scape_string)
 
+
+# Formateo
+
+name, surname, age = "Brais", "Moure", 35
+
+print("Mi nombre es {} {} y mi edad es {}".format(name, surname, age))
+print("Mi nombre es %s %s y mi edad es %d" %(name, surname, age))
+print(f"Mi nombre es {name} {surname} y mi edad es {age}")
+
