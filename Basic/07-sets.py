dd99bcdf34ad718837876aa52ea6b2b8b5705d6a
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-16 12:39:47 -0500

    Set - inicializacion - len

--- Basic/07-sets.py
+++ Basic/07-sets.py
@@ -11,3 +11,12 @@ my_other_set = {}
 print(type(my_set))
 print(type(my_other_set))   # esto nos dice que inicialmente es un diccionario
 
+my_other_set = {"Brais","Moure", 35}
+print(type(my_other_set))   # dice que ya no es diccionario, ahora es SETs
+
+print(len(my_other_set)) # cunta los elementos del SETS
+my_other_set.add("MoureDev")    # agrega el dato "MoureDev" en posiciones aleatorias
+my_other_set.add("MoureDev")    # no permite valores repetidos
+
+print(my_other_set)
+
