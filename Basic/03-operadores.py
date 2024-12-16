b0811db7dcdb2a4a96177dbea39d9e5698770e8d
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-16 12:13:18 -0500

    operators - symbols

---
+++ Basic/03-operadores.py
@@ -0,0 +1,18 @@
+# OPERADORES 
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(10 % 2)
+print(10 // 3) # nos da un resultado aprox a un entero 
+print(2 ** 3) # potenciacion
+print(2 ** 3 + 3 / 7 / 1 // 4)
+print("Hola " + "Python")
+print("Hola " + "Python " + "Que tal?")
+print("Hola " + str(5))
+print("Hola " * 5) # repite5 veces Hola
+print("Hola" * (2 ** 3)) # repite 8 veces Hola
+
+my_float = 2.5 * 2
+print("Hola" * int(my_float)) # convertimos el 'float' a un entero 'int'
+
