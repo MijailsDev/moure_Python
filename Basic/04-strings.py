7574fa8b16c473773cfad50a89dc8c348284e4b5
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-16 12:16:51 -0500

    String - Desempaquetado de caracteres

--- Basic/04-strings.py
+++ Basic/04-strings.py
@@ -29,3 +29,10 @@ print("Mi nombre es {} {} y mi edad es {}".format(name, surname, age))
 print("Mi nombre es %s %s y mi edad es %d" %(name, surname, age))
 print(f"Mi nombre es {name} {surname} y mi edad es {age}")
 
+
+# Desempaquetado de caracteres
+language = "Python"
+a, b, c, d, e, f = language
+print(a)
+print(b)
+
