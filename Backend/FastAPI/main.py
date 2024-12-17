c8e44fcb054bbe315449074e6c7d7bbe1fcdb0c6
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-17 11:48:28 -0500

    FastAPI - update main.py

--- Backend/FastAPI/main.py
+++ Backend/FastAPI/main.py
@@ -3,11 +3,13 @@ from fastapi import FastAPI     # importarmos fastAPI
 # primer hola fastAPI con el servidor
 app = FastAPI()
 
+# Peticiones get
 @app.get("/")       # va a la raiz del localhost - solo debe de existir una raiz(/)
 async def root():
     return "Hola FastAPI!"
 
 
 @app.get("/url")    # esto va a estar el 127.0.0.1:8000/url
-async def root():
-    return {"url_curso":"https://mouredev.com/python"}
+async def url():    # ponemos un nombre distinto a root a la funcion
+    return {"url":"https://mouredev.com/python"}
+
