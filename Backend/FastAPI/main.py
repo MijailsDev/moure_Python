9f155a07232c93449d9e1fbb30cd275113885037
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-17 11:41:30 -0500

    Fas API - url del curso

--- Backend/FastAPI/main.py
+++ Backend/FastAPI/main.py
@@ -3,6 +3,11 @@ from fastapi import FastAPI     # importarmos fastAPI
 # primer hola fastAPI con el servidor
 app = FastAPI()
 
-@app.get("/")
+@app.get("/")       # va a la raiz del localhost - solo debe de existir una raiz(/)
 async def root():
     return "Hola FastAPI!"
+
+
+@app.get("/url")    # esto va a estar el 127.0.0.1:8000/url
+async def root():
+    return {"url_curso":"https://mouredev.com/python"}
