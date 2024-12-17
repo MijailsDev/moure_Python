19ef12659a3dfba84fe134c6374e1236a0520370
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-17 12:29:02 -0500

    FastAPI - detener el server

--- Backend/FastAPI/main.py
+++ Backend/FastAPI/main.py
@@ -13,3 +13,13 @@ async def root():
 async def url():    # ponemos un nombre distinto a root a la funcion
     return {"url":"https://mouredev.com/python"}
 
+
+"""
+las peticiones GET son las que interpretan el navegador
+GET = obtener
+
+POSTMAN = es un cliente para poder pedir peticiones a un API
+POSTMAN = para interactuar con un backend
+
+VS code Pluggins -> Thunder Client (alternativa a Postman)
+"""
--- Backend/instalacion_FastAPI.sh
+++ Backend/instalacion_FastAPI.sh
@@ -11,8 +11,14 @@ https://fastapi.tiangolo.com/tutorial/first-steps/
 # levantamos el servidor
 uvicorn main:app --reload 
 
-# Documentacion de FastAPI en tu servidor
+## Documentacion de FastAPI en tu servidor
+
+# Documentacion con Swagger 
 http://127.0.0.1:8000/docs
 
+# Documentacion con Redocly
 http://127.0.0.1:8000/redoc
+
+# Como detener tu Servidor
+Ctrl + C
 
