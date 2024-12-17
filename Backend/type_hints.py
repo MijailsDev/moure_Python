fddb241b48233f1c6fe4b4b3862a913b643b9dbf
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-17 09:56:50 -0500

    Type hints - aplicacion de tipos de datos a las varaibles

--- Backend/type_hints.py
+++ Backend/type_hints.py
@@ -8,3 +8,12 @@ my_string_variable = 5
 print(my_string_variable)
 print(type(my_string_variable))
 
+
+my_typed_variable = str: "My typed String variable"     # declaramos que esta variable sea de tipo string
+print(my_typed_variable)
+print(type(my_string_variable))
+
+
+my_typed_variable = 5   # aunque hemos especificadode tipo string no hace efecto, porque es tipado debil
+print(my_typed_variable)
+print(type(my_string_variable))
