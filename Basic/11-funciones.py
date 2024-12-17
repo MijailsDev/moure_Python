91d54d60e79a5819978e6bb44416493597d74808
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-17 08:08:11 -0500

    Funciones - Declaracion con parametros

--- Basic/11-funciones.py
+++ Basic/11-funciones.py
@@ -9,3 +9,14 @@ my_function()
 my_function()
 my_function()
  
+# declaracion de funciones con parametros 
+def sum_two_values (first_number, second_number):
+    print(first_number + second_number)
+
+sum_two_values(5,7)
+sum_two_values(53,27)
+sum_two_values("5","7")
+sum_two_values(1.4,5.2)
+
+# Nota Importante : parametros y argumentos son iguales
+
