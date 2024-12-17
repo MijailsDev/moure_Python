2b190bf7ea2b594e0213949c43235e03bc75e605
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-17 11:11:53 -0500

    FastAPI - levantamos el servidor

--- Backend/FastAPI/main.py
+++ Backend/FastAPI/main.py
@@ -1,1 +1,7 @@
 from fastapi import FastAPI     # importarmos fastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return "Hola FastAPI!"
--- Backend/instalacion_FastAPI.sh
+++ Backend/instalacion_FastAPI.sh
@@ -1,4 +1,4 @@
-"""
+
 FastAPI  > nos proporciona nuestro propio servidor llamado "uvicorn"
 
 # instalamos fastAPI con todas sus dependencias
@@ -7,5 +7,8 @@ pip install "fastapi[all]"
 # Primeros pasos luego de instalar fast API
 https://fastapi.tiangolo.com/tutorial/first-steps/
 
-"""
+
+# levantamos el servidor
+uvicorn main:app --reload 
+
 
