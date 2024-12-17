83b35bd6fd58516ccee5d6a8da7c5b146050f519
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-17 08:04:11 -0500

    Bucles - For

--- Basic/10-bucles.py
+++ Basic/10-bucles.py
@@ -22,4 +22,35 @@ while my_condition < 20:
         print("se detien la ejecucion: break")
         break
     print(my_condition)
-    
+
+
+
+# For 
+
+my_list = [35,24,62,52,30,30,17]
+
+for element in my_list:
+    print(element)
+
+my_set = {"Brais","Moure", 35}
+
+for element in my_set:
+    print(element)
+
+my_dict = {"Nombre":"Brais","Apellido":"Moure","Edad":35,1:"Python"}
+
+for element in my_dict:
+    print(element)
+    if element == "Edad":
+        break
+    print("Se ejecuta")
+else:
+    print("El else del bucle for dict ")
+
+for element in my_dict:
+    print(element)
+    if element == "Edad":
+        continue            # esto deja de ejecutar hasta esta linea, y comienza desde el inicio de este bucle :(
+    print("Se ejecuta")
+else:
+    print("El else del bucle for dict ")
