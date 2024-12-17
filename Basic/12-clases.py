f9e08761b5efb44bddcee78f3637a45364e7fd17
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-17 08:21:40 -0500

    Clases - Usos que se le puede dar

--- Basic/12-clases.py
+++ Basic/12-clases.py
@@ -10,9 +10,54 @@ print(MyEmptyPerson())
 
 class Person:
     def __init__(self, name, surname):     # tenemos un constructor de clase
-        self.name = name
-        self.surname = surname 
+        self.full_name = f"{name} {surname}"
+
+    def walk(self):
+        print(f"{self.full_name} Esta caminado")
+
 
 my_person = Person("Brais","Moure")
+print(my_person.full_name)
+my_person.walk()
 
-print(f"{my_person.name} {my_person.surname}")
+
+
+# --------------------------------------------------------------
+### CLASES ### 
+
+# declaracion de clases
+print("_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_")
+
+class MyEmptyPerson2:
+    pass 
+
+print(MyEmptyPerson2)
+print(MyEmptyPerson2())
+
+class Person2:
+    def __init__(self, name, surname, alias = "sin alias"):     # tenemos un constructor de clase
+        self.full_name = f"{name} {surname} ({alias})"  # Propiedad publica
+        self.__name = name  # Propiedad privada
+        
+
+    def get_name(self):
+        return self.__name
+    
+    def walk(self):
+        print(f"{self.full_name} Esta caminado")
+
+
+
+my_person = Person2("Brais","Moure")
+print(my_person.full_name)
+print("Hola ",my_person.get_name())
+my_person.walk()
+
+my_other_person = Person2("Brais", "Moure", "MoureDev")
+print(my_other_person.full_name)
+my_other_person.walk()
+my_other_person.full_name = "Hector de Leon (El loco de los perros)"
+print(my_other_person.full_name)
+
+my_other_person.full_name = 666
+print(my_other_person.full_name)
