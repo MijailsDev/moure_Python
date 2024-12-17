7f474101d17e21a4b2003727e10681ae6c8d50fd
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-17 10:42:38 -0500

    FastAPI - link primeros pasos

--- Backend/instalacion_FastAPI.py
+++ Backend/instalacion_FastAPI.py
@@ -4,7 +4,8 @@ FastAPI  > nos proporciona nuestro propio servidor llamado "uvicorn"
 # instalamos fastAPI con todas sus dependencias
 pip install "fastapi[all]"
 
-
+# Primeros pasos luego de instalar fast API
+https://fastapi.tiangolo.com/tutorial/first-steps/
 
 """
 
